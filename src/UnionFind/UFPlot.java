4792eca55296b68fac634b955e98fe3dd89692f1
muz0xd <muz0xd@gmail.com>
2022-10-19 22:47:13 +0300

    formatter test

--- src/UnionFind/UFPlot.java
+++ src/UnionFind/UFPlot.java
@@ -12,56 +12,56 @@ public class UFPlot {
 			
 	public UFPlot (int n) {
 	  uf = new ArrayList<UFind>();
-		uf.add(new QuickFindUF(n));           //0
-		uf.add(new QuickUnionUF(n));          //1
-		uf.add(new QuickWeightedUF(n));       //2
+	  uf.add(new QuickFindUF(n));           //0
+	  uf.add(new QuickUnionUF(n));          //1
+	  uf.add(new QuickWeightedUF(n));       //2
 	}
 	
 	public void printGraph() {
-		StdDraw.setScale(0, 1300);
-		StdDraw.line(10, 10, 10, 1200);
-		StdDraw.line(10, 10, 1200, 10);
-		StdDraw.setPenRadius(0.005);
+	  StdDraw.setScale(0, 1300);
+	  StdDraw.line(10, 10, 10, 1200);
+	  StdDraw.line(10, 10, 1200, 10);
+	  StdDraw.setPenRadius(0.005);
 	}
 	
 	public static void main(String[] args) {
-		int n = StdIn.readInt();
-		UFPlot plot = new UFPlot(n);
-		UFind UfEntity;
-		plot.printGraph();
-		
-		try {
-		    UfEntity = plot.uf.get(0);  // algorithm select 
+	  int n = StdIn.readInt();
+	  UFPlot plot = new UFPlot(n);
+	  UFind UfEntity;
+	  plot.printGraph();
+	  
+	  try {
+	    UfEntity = plot.uf.get(0);  // algorithm select 
+	  }
+	  catch (ClassCastException e) {
+	    throw new ClassCastException("UFind: Inappropriate cast to specific type");
+	  }
+	  catch (IndexOutOfBoundsException e) {
+	    throw new IndexOutOfBoundsException("no such index in algorithm list");
 		}
-		catch (ClassCastException e) {
-			throw new ClassCastException("UFind: Inappropriate cast to specific type");
-		}
-		catch (IndexOutOfBoundsException e) {
-			throw new IndexOutOfBoundsException("no such index in algorithm list");
-		}
-		
-		while (!StdIn.isEmpty()) {
-			int p, q;
-			
-			try {
-				p = StdIn.readInt();
-				q = StdIn.readInt();
-			}
-			catch (NoSuchElementException e) {
-				throw new NoSuchElementException("Mo more int elements are avialable");
-			}
-			
-			if (!UfEntity.connected(p, q)) {
-				UfEntity.union(p, q);
-				StdDraw.setPenColor(UfEntity.getColor());
-		    StdDraw.point(UfEntity.getI() + 10, UfEntity.getCost() + 10);  // number of array accesses
-			}
-			
-			StdDraw.setPenColor(StdDraw.GREEN);	
-			StdDraw.point(UfEntity.getI() + 10, 
-			    (double) UfEntity.getTotal()/UfEntity.getI() + 10);  // average number of array accesses 
-			//System.out.println(UfEntity.getI() + "," + 
+	  
+	  while (!StdIn.isEmpty()) {
+	    int p, q;
+	    
+	    try {
+	      p = StdIn.readInt();
+	      q = StdIn.readInt();
+	    }
+	    catch (NoSuchElementException e) {
+	      throw new NoSuchElementException("Mo more int elements are avialable");
+	    }
+	    
+	    if (!UfEntity.connected(p, q)) {
+	      UfEntity.union(p, q);
+	      StdDraw.setPenColor(UfEntity.getColor());
+	      StdDraw.point(UfEntity.getI() + 10, UfEntity.getCost() + 10);  // number of array accesses
+	    }
+	    
+	    StdDraw.setPenColor(StdDraw.GREEN);	
+	    StdDraw.point(UfEntity.getI() + 10, 
+	        (double) UfEntity.getTotal()/UfEntity.getI() + 10);  // average number of array accesses 
+	    //System.out.println(UfEntity.getI() + "," + 
 	    //UfEntity.getCost() + ","+ UfEntity.getTotal()/UfEntity.getI());
-		}
+	  }
 	}
 }
