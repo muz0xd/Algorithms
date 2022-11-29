df3a32c17aa46fcf8e7ba14c3e26522d8ec3e9af
muz0xd <muz0xd@gmail.com>
2022-11-29 22:05:41 +0300

    shellsort comment

--- src/Sort/Shell.java
+++ src/Sort/Shell.java
@@ -3,13 +3,14 @@ package Sort;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 
+//Taking every h-th entry yields a sorted subsequence. Array to be h-sorted. h-value from, for example, 364 ... to 1
 public class Shell {
     public static <T extends Comparable<T>>  void sort (T[] a) {
         int N = a.length;
         int h =1;
-        while (h < N/3) h = 3*h + 1;
-        while (h >= 1) {
-            for (int i = h; i < N; i++) {
+        while (h < N/3) h = 3*h + 1;        // 1, 4, 13, 40, 121, 364, 1093 ..   
+        while (h >= 1) {                    // h-sort the array
+            for (int i = h; i < N; i++) {   // Insert a[i] among a[i-h], a[i-2h], a[i-3h].. . 
                 for (int j = i; j >= h && less(a[j], a[j-h]); j -= h)
                     exch(a, j, j-h);
             }
