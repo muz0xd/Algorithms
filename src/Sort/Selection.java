f2886d6c949c242c075c0b38d93b9007c607205c
muz0xd <muz0xd@gmail.com>
2022-11-29 22:20:27 +0300

    file formatting

--- src/Sort/Selection.java
+++ src/Sort/Selection.java
@@ -6,42 +6,42 @@ import edu.princeton.cs.algs4.StdOut;
 
 public class Selection {
     
-	public static <T extends Comparable<T>>  void sort (T[] a) {
-	  int N = a.length;
-	  for (int i = 0; i < N; i++) { // Exchange a[i] with smallest entry in a [i+1..N) 
-	    int min = i; // index of minimal entry
-	    for (int j = i+1; j < N; j++)
-	      if (less(a[j], a[min])) min = j;
-	    exch(a, i, min);
-		};
-	}
+    public static <T extends Comparable<T>>  void sort (T[] a) {
+        int N = a.length;
+        for (int i = 0; i < N; i++) { // Exchange a[i] with smallest entry in a [i+1..N) 
+            int min = i; // index of minimal entry
+            for (int j = i+1; j < N; j++)
+                if (less(a[j], a[min])) min = j;
+            exch(a, i, min);
+            };
+    }
 
-	private static <T extends Comparable<T>> boolean less (T v, T w) {
-	  return v.compareTo((T) w) < 0;
-	}
+    private static <T extends Comparable<T>> boolean less (T v, T w) {
+        return v.compareTo((T) w) < 0;
+    }
 	
-	private static <T extends Comparable<T>> void exch (T[] a, int i, int min) {
-	  T t = a[i];
-	  a[i] = a[min];
-	  a[min] = t;
-	}
+    private static <T extends Comparable<T>> void exch (T[] a, int i, int min) {
+        T t = a[i];
+        a[i] = a[min];
+        a[min] = t;
+    }
 	
-	private static <T extends Comparable<T>> void show (T[] a) {
-	  for (int i = 0; i < a.length; i++) 
-	    StdOut.print(a[i] + " ");
-	  StdOut.println();
-	}
+    private static <T extends Comparable<T>> void show (T[] a) {
+        for (int i = 0; i < a.length; i++) 
+            StdOut.print(a[i] + " ");
+        StdOut.println();
+    }
 	
-	public static <T extends Comparable<T>> boolean isSorted (T[] a) {
-	  for (int i = 0; i < a.length; i++)
-	    if (less(a[i], a[i+1])) return false;
-	  return true;
-	}
+    public static <T extends Comparable<T>> boolean isSorted (T[] a) {
+        for (int i = 0; i < a.length; i++)
+            if (less(a[i], a[i+1])) return false;
+        return true;
+    }
 	
-	public static void main(String[] args) {
-	  String[] a = new In().readAllStrings();
-	  sort(a);
-	  assert isSorted(a);
-	  show(a);
-	}
+    public static void main(String[] args) {
+        String[] a = new In().readAllStrings();
+        sort(a);
+        assert isSorted(a);
+        show(a);
+    }
 }
