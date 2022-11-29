048623f7180bc781176cca48a1fdb0b29aa3f9f1
muz0xd <muz0xd@gmail.com>
2022-11-29 22:12:31 +0300

    selection sort comment

--- src/Sort/Selection.java
+++ src/Sort/Selection.java
@@ -8,8 +8,8 @@ public class Selection {
     
 	public static <T extends Comparable<T>>  void sort (T[] a) {
 	  int N = a.length;
-	  for (int i = 0; i < N; i++) {
-	    int min = i;
+	  for (int i = 0; i < N; i++) { // Exchange a[i] with smallest entry in a [i+1..N) 
+	    int min = i; // index of minimal entry
 	    for (int j = i+1; j < N; j++)
 	      if (less(a[j], a[min])) min = j;
 	    exch(a, i, min);
